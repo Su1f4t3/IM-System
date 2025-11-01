bf04c617074f52a66aeef2f85357dd85c5c78c5c
liu <2394917073@qq.com>
2025-11-01 11:52:22 +0800

    V0.6修改用户名

---
+++ server.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// 定义服务器结构体
+type Server struct {
+	Ip        string
+	Port      int
+	OnlineMap map[string]*User // 在线用户列表
+	maplock   sync.RWMutex     // 读写锁，保护并发访问
+	Message   chan string      // 消息广播channel
+}
+
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+// 监听消息（Message）广播channel的goroutine，一旦有消息，就将消息发送给所有在线用户（User）的channel
+func (s *Server) ListenMessager() {
+	for {
+		msg := <-s.Message
+
+		// 将msg发送给所有在线的User
+		s.maplock.Lock()
+		for _, cli := range s.OnlineMap {
+			cli.C <- msg
+		}
+		s.maplock.Unlock()
+	}
+}
+
+// 广播消息方法
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ": " + msg
+
+	s.Message <- sendMsg
+}
+
+func (s *Server) Handler(conn net.Conn) {
+
+	// 创建新用户对象
+	user := NewUser(conn, s)
+
+	// 用户上线业务
+	user.Online()
+
+	// 接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096) // 创建一个 4096 字节的缓冲区（切片）用于存储从客户端接收的数据
+
+		// 无限循环等待客户端发送消息
+		for {
+			n, err := conn.Read(buf) // 会阻塞直到接收到数据或连接断开
+
+			// 如果 n 等于 0，表示客户端断开连接
+			if n == 0 {
+				user.Offline()
+				return
+			}
+
+			if err != nil {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			msg := string(buf[:n-1]) // 去掉换行符 "\n"
+
+			// 用户针对msg进行消息处理
+			user.DoMessage(msg)
+		}
+	}()
+
+	// 当前handler阻塞
+	select {}
+}
+
+// 启动服务器的接口
+func (s *Server) Start() {
+	// 创建一个 TCP 网络监听
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	// 关闭监听器
+	defer listener.Close()
+
+	// 启动监听Message的goroutine
+	go s.ListenMessager()
+
+	// 无限循环等待客户端连接
+	for {
+		// 接受客户端连接
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue // 继续等待下一个连接
+		}
+
+		// 为每个连接启动独立的处理goroutine
+		go s.Handler(conn)
+	}
+}
---
+++ user.go
@@ -0,0 +1,104 @@
+package main
+
+import "net"
+
+type User struct {
+	Name string      // 用户名（默认使用连接地址）
+	Addr string      // 用户地址
+	C    chan string // 接收消息的channel
+	conn net.Conn    // 网络连接对象
+
+	server *Server // 当前用户所属的服务器
+}
+
+// 创建一个用户的接口
+func NewUser(conn net.Conn, server *Server) *User {
+	// 获取客户端连接地址作为用户名和地址
+	userAddr := conn.RemoteAddr().String()
+
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+
+		server: server,
+	}
+
+	// 启动监听当前user channel消息的goroutine
+	go user.ListenMessage()
+
+	return user
+}
+
+// 用户上线业务
+func (u *User) Online() {
+
+	// 用户上线，将用户加入在线用户列表（OnlineMap）中
+	u.server.maplock.Lock()
+	u.server.OnlineMap[u.Name] = u
+	u.server.maplock.Unlock()
+
+	// 广播当前用户上线消息
+	u.server.BroadCast(u, "已上线")
+}
+
+// 用户下线业务
+func (u *User) Offline() {
+
+	// 用户下线，将用户从在线用户列表（OnlineMap）中移除
+	u.server.maplock.Lock()
+	delete(u.server.OnlineMap, u.Name)
+	u.server.maplock.Unlock()
+
+	// 广播当前用户下线消息
+	u.server.BroadCast(u, "已下线")
+}
+
+// 给当前user对应的客户端发送消息
+func (u *User) sendMsg(msg string) {
+	u.conn.Write([]byte(msg))
+}
+
+// 用户处理消息业务
+func (u *User) DoMessage(msg string) {
+	if msg == "who" {
+
+		// 查询当前在线用户都有哪些
+		u.server.maplock.Lock()
+		for _, user := range u.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ": 在线...\n"
+			u.sendMsg(onlineMsg)
+		}
+		u.server.maplock.Unlock()
+
+	} else if len(msg) > 7 && msg[0:7] == "rename|" {
+		// 消息格式：rename|张三
+		newName := msg[7:]
+
+		// 判断name是否存在
+		_, ok := u.server.OnlineMap[newName]
+		if ok {
+			u.sendMsg("当前用户名已被使用\n")
+		} else {
+			u.server.maplock.Lock()
+			delete(u.server.OnlineMap, u.Name) // 删除原来的用户名
+			u.server.OnlineMap[newName] = u    // 添加新用户名
+			u.server.maplock.Unlock()
+
+			u.Name = newName
+			u.sendMsg("用户名更新成功，新的用户名为：" + u.Name + "\n")
+		}
+	} else {
+		u.server.BroadCast(u, msg)
+	}
+}
+
+// 监听用户（User）消息 channel ，一旦有消息就发送给客户端
+func (u *User) ListenMessage() {
+	for {
+		msg := <-u.C
+
+		u.conn.Write([]byte(msg + "\n"))
+	}
+}
